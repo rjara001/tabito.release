61f43219ab625e78da18f8035a893c841f77e82e
rjara001 <rjara001@yahoo.com>
2024-07-16 12:21:40 +0000

    Deploying to gh-pages from  @ 20db6dfb5cf4a1dfff28e3f3b0b93fc1cfbe11d7 🚀

--- js/util.js
+++ js/util.js
@@ -6,3 +6,21 @@ function saveAsFile(fileName, byteBase64) {
     link.click();
     document.body.removeChild(link);
 };
+
+window.clickOutsideHandler = {
+    initialize: function (element, dotNetHelper) {
+        function handleClick(event) {
+            if (!element.contains(event.target)) {
+                dotNetHelper.invokeMethodAsync('HandleClickOutside');
+            }
+        }
+
+        document.addEventListener('click', handleClick);
+
+        return {
+            dispose: function () {
+                document.removeEventListener('click', handleClick);
+            }
+        };
+    }
+};
