7308b59fb66d4f71980f076ef497871a9905ed4d
Rodrigo Jara <rodrigo.jara@raisin.com>
2024-08-04 22:53:05 +0200

    adding mobile

--- js/sidebar.js
+++ js/sidebar.js
@@ -1,6 +1,6 @@
 ﻿
 $(function(){
-    const mobileSizeQuery = window.matchMedia('(max-width: 767.98px)');
+    const mobileSizeQuery = window.matchMedia('(max-width: 768px)');
    mobileSizeQuery.addListener(handleMobileSizeChange);
 
    // Initial check
@@ -23,10 +23,18 @@ $(function(){
 
 function handleMobileSizeChange(e) {
    if (e.matches) {
+       //window.isMobile = true;
+       $('#userName').addClass('disabled');
+       $('#userLogo').removeClass('disabled');
+
        $('.bottom-bar').removeClass('disabled');
        $('.wrapper').addClass('mobile');
    // Add any additional actions here
    } else {
+       //window.isMobile = false;
+       $('#userLogo').addClass('disabled');
+       $('#userName').removeClass('disabled');
+
        $('.bottom-bar').addClass('disabled');
        $('.wrapper').removeClass('mobile');
    // Add any additional actions here
